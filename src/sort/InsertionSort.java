7b8593101c091eaf56faa74f5b5ce13d3f9fd917
vina <vinaybalepur@gmail.com>
2017-09-14 09:42:06 +0530

    Added notes to sorting

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -1,10 +1,20 @@
 package sort;
 
+import java.util.Arrays;
+
+/**
+ * 
+ * @author vinay
+ * In insertion sort the values to the current value should be sorted. The assumption is 0th index is already sorted. Start the search from
+ * 1st index. The key value is the current value that should be sorted. If the pointer, j, is equal to greater than 0 and if key is less then 
+ * array[j] then the values are sorted and j is reduced by 1. This happens till j becomes lesser than 0. i.e the value is placed at the 0th index
+ * or till key is greater than current index value. Here number of swaps is reduced greatly increasing the speed of sort.
+ */
 public class InsertionSort 
 {
 	public static void main(String[] args) 
 	{
-		int a[] = {3,4,1,5,2,-5,3};
+		int a[] = {4,3,1,5,2,-5,3};
 		int  key,j, temp;
 		
 		for(int i=1;i<a.length;i++)
@@ -21,10 +31,7 @@ public class InsertionSort
 		}
 		
 		
-		for(int i=0;i<a.length;i++)
-		{
-			System.out.println(a[i]);
-		}
+		System.out.println(Arrays.toString(a));
 	}
 	
 	
--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -1,5 +1,14 @@
 package sort;
 
+import java.util.Arrays;
+
+/**
+ * Here we swap the lowest element in the array with the element in the 0th position. We swap the next lowest element with the current element.
+ * So initially 10 is swapped with -5 since -5 is the lowest. Then 0 is swapped with 5, 1 is swapped with 7. By this point the entire list is 
+ * sorted.  
+ * @author vinay
+ *
+ */
 public class SelectionSort
 {
 	
@@ -24,10 +33,7 @@ public class SelectionSort
 			x[indexPointer] = temp;
 		}
 		
-		for(int i=0;i<x.length;i++)
-		{
-			System.out.println(x[i]);
-		}
+		System.out.println(Arrays.toString(x));
 	}
 
 }
